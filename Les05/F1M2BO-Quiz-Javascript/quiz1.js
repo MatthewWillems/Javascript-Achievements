a84f6bbb4760c698736703d18bc6e875921db27e
Matthew Willems <matthewwillems2003@outlook.com>
2021-01-21 23:11:44 +0100

    quiz

---
+++ Les05/F1M2BO-Quiz-Javascript/quiz1.js
@@ -0,0 +1,77 @@
+const quiz1 =
+{
+    "quizID": "Museum 1",
+    "quizMetaData":
+    {
+        "title": "De ruimte",
+        "imageURI": "https://pngimg.com/uploads/question_mark/question_mark_PNG134.png"
+
+    },
+    "quizContent":		
+		[
+			{
+				"question": "Wat is geen dwergplaneet?",
+				"answers": [{
+						"answer": "Alpha Centauri",
+						"feedback": true
+					},
+					{
+						"answer": "Makemake",
+						"feedback": false
+					},
+					{
+						"answer": "Ceres",
+						"feedback": false
+					},
+					{
+						"answer": "Pluto",
+						"feedback": false
+					},
+					{
+						"answer": "Orcus",
+						"feedback": false
+					}
+				]
+			},
+			{
+				"question": "De eerste mens op de Maan was?",
+				"answers": [{
+						"answer": "Chris Hadfield",
+						"feedback": false
+					},
+					{
+						"answer": "Neil Armstrong",
+						"feedback": true
+					},
+					{
+						"answer": "Buzz Aldrin",
+						"feedback": false
+					},
+					{
+						"answer": "André Kuipers",
+						"feedback": false
+					}
+				]
+			},
+			{
+				"question": "Een lichtjaar is een eenheid van?",
+				"answers": [{
+						"answer": "Tijd",
+						"feedback": false
+					},
+					{
+						"answer": "Gewicht",
+						"feedback": false
+					},
+					{
+						"answer": "Snelheid",
+						"feedback": false
+					},
+					{
+						"answer": "Afstand",
+						"feedback": true
+					}
+				]
+			}
+		]
+	}
